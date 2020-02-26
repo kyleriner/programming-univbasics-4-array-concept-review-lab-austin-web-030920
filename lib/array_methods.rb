f76e10823ac61a1ff43c9c1567eba19d8f00eb04
Kyle Riner <rinerkyle97@gmail.com>
2020-02-26 17:14:52 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,8 @@
 def find_element_index(array, value_to_find)
   array.length.times { |index|
-    if array[index] do {
+    if array[index] == value_to_find
       puts "This works"
-    }
+    
   }
 end
 
